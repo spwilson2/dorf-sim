67153150d22f2537eb979675c495a6f58a11ada1
Sean Wilson <spwilson27@gmail.com>
2023-05-27 17:46:14 -0700

    Working example of movement along intended path

--- dorf-lib/src/script/mod.rs
+++ dorf-lib/src/script/mod.rs
@@ -16,30 +16,121 @@ impl Plugin for ScriptPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(handle_camera_movement_keys)
             .add_system(handle_camera_resized)
+            .add_system(system_assign_optimal_path)
+            .add_system(system_move_on_optimal_path)
+            .add_startup_system(spawn_mv_player)
             .add_startup_system(spawn_textures);
     }
 }
 
+// TODO Convert the TextureRect to ths.
+// #[derive(Component)]
+// struct Transform {
+//     pub position: Vec3,
+//     pub size: Vec3,
+// }
+
+#[derive(Component)]
+struct MovePath {
+    steps: Vec<Vec2>,
+}
+
+// TODO: Add these to the move algo.
+// . #[derive(Component)]
+// struct Collider {
+//     transform: Transform,
+// }
+
+#[derive(Component)]
+struct GoalLoc ( Vec2);
+#[derive(Component)]
+struct Speed (f32 );
+
+#[derive(Bundle)]
+struct Player {
+    speed: Speed,
+    goal: GoalLoc,
+    rect: TextureRect,
+}
+
+fn system_assign_optimal_path(
+    mut cmd: Commands, 
+    q: Query<(Entity, &GoalLoc)>,
+) {
+    for (entity, goal) in q.iter() {
+        // TODO: Calculate optimal path, for now will work since nothing to
+        // colldie with. Will need to figure out how to do efficient collision
+        // detec later.
+        cmd.entity(entity).insert(MovePath{
+            steps: vec![goal.0]
+        }).remove::<GoalLoc>();
+    }
+}
+
+fn system_move_on_optimal_path(
+    mut cmd: Commands, 
+    time: Res<Time>,
+    mut q: Query<(Entity, &mut MovePath, &mut TextureRect, &Speed)>,
+) {
+    log::info!("in move optimal");
+    for (entity, mut path, mut rect, speed) in q.iter_mut() { 
+        let mut travel = speed.0 * time.delta().as_secs_f32();
+        loop { // While we have time to travel, contiue doing so.
+
+            // First check if there's nothing left to move
+            if path.steps.is_empty() {
+                cmd.entity(entity).remove::<MovePath>();
+                // TODO: Add another goal path
+                break;
+            }
+
+            let dist = rect.loc.distance(*path.steps.last().unwrap());
+            if dist <= travel {
+                travel -= dist;
+                // We've move to the point, need to continue to the next point.
+                rect.loc = path.steps.pop().unwrap();
+            } else {
+                // We can't move directly to the point, let's get as close as we can
+                let direction = (*path.steps.last().unwrap() - rect.loc).normalize();
+                rect.loc += direction * travel; 
+                break;
+            }
+        }
+    }
+}
+
+fn spawn_mv_player(mut cmd: Commands) {
+    cmd.spawn(Player { 
+        speed: Speed(1.5),
+        goal: GoalLoc(Vec2::new(10.0, 10.0)),
+        rect: TextureRect{
+        texture: 'p',
+        dim: Vec2::new(1.0, 1.0),
+        loc: Vec2::new(0.0, 0.0),
+        loc_z: 2.0,
+        }});
+    }
+/// - Create a player with a random intended path
+///    - new entity: 
+///         - new CurLocation
+///         - new GoalLocation
+/// - Get the actual steps to take to get to that path using a*
+///    - ref CurLocation
+///    - del GoalLocation
+///    - new TargetLocation
+/// - Move the player to that position along the routed path
+///    - mut CurLocation
+///    - 
+/// - Player at destitation, then give them a new random path
+
+
 fn spawn_textures(mut cmd: Commands) {
     cmd.spawn(TextureRect {
         texture: 'a',
-        dim: Vec2::new(2.0, 1.0),
+        dim: Vec2::new(1.0, 1.0),
         loc: Vec2::new(0.0, 0.0),
         loc_z: 1.0,
     });
-    //cmd.spawn(TextureRect {
-    //    texture: 'b',
-    //    dim: Vec2::new(1.0, 1.0),
-    //    loc: Vec2::new(0.0, 0.0),
-    //    loc_z: 2.0,
-    //});
-    //cmd.spawn(TextureRect {
-    //    texture: '.',
-    //    dim: Vec2::new(1000.0, 1000.0),
-    //    loc: Vec2::new(0.0, 0.0),
-    //    loc_z: 1.0,
-    //});
-
     let vert_wall = TextureRect {
         texture: '-',
         dim: Vec2::new(1.0, 2.0),
